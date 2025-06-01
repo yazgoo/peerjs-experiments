c9f1980a976025be2e0b69b9dec03f8e9b2ed0fc
yazgoo <yazgoo@gmail.com>
2025-06-01 22:46:20 +0200

    user management

--- log.js
+++ log.js
@@ -22,3 +22,13 @@ function clear() {
 function cleanHTML(str) {
     return new DOMParser().parseFromString(str, "text/html").body.textContent;
 }
+
+function userHTML(user) {
+    // genererate a color base on the username
+    let hash = 0;
+    for (let i = 0; i < user.length; i++) {
+        hash = user.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let color = '#' + ((hash & 0x00FFFFFF).toString(16).padStart(6, '0'));
+    return '<span style="font-weight: bold; color: ' + color + ';">' + cleanHTML(user) + '</span>';
+}
--- room.js
+++ room.js
@@ -1,11 +1,24 @@
 const isServer = !location.hash || location.hash === "";
 const connections = [];
+let peerId = null;
+let users = {};
+
+function handleWithUsers(data) {
+    handle(data);
+    if(users[data.peerId] && users[data.peerId] === data.usr) {
+        return;
+    }
+    users[data.peerId] = data.usr;
+    onUsersUpdate(users);
+}
+
 /* main net logic */
 
 if(isServer) {
     const roomName = crypto.randomUUID();
     const peer = new Peer(roomName);
     peer.on('open', id => {
+        peerId = id;
         const newUrl = location.href + "#" + roomName;
         logi("please share the following URL: <a href='" + newUrl + "'>" + newUrl + "</a>");
     });
@@ -13,7 +26,7 @@ if(isServer) {
         logi("New connection: " + conn.peer);
         connections.push(conn);
         conn.on('data', data => {
-            handle(data);
+            handleWithUsers(data);
             connections.forEach(c => {
                 if (c !== conn) c.send(data);
             });
@@ -22,21 +35,27 @@ if(isServer) {
 } else {
     const peer = new Peer();
     peer.on('open', id => {
+        peerId = id;
         const roomName = location.hash.replace("#", "");
         logi("starting client with ID: " + id);
         const conn = peer.connect(roomName);
         conn.on('open', () => {
             logi("Connected to host");
-            conn.on('data', data => handle(data));
+            conn.on('data', data => handleWithUsers(data));
             connections.push(conn);
         });
     });
 }
 
 window.send = (what) => {
+    if(!usr.value || usr.value.trim() === "") {
+        alert("Please enter a username");
+    }
     let payload = buildPayload(what);
     if(payload !== null) {
-        handle(payload);
+        payload['peerId'] = peerId;
+        payload['usr'] = usr.value;
+        handleWithUsers(payload);
         connections.forEach(c => c.send(payload));
     }
 };
